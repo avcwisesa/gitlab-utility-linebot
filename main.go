b5c204e49dfafe2f2cdbd2a42ff0d2a8a218f61a
Avicenna <avcwisesa@gmail.com>
2018-04-12 09:01:00 +0700

    add dependency & update env.example

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bot, err := linebot.New(os.Getenv("LINE_CHANNEL_SECRET"), os.Getenv("LINE_CHANNEL_TOKEN"))
+
 	fmt.Println("Hello World :)")
 }
